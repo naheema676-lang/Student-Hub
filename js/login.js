5c042cba9d2dc7546062e3c554fe1e7837c3f9a6
Ali <naheema676@gmail.com>
2026-06-27 09:20:40 -0700

    folder changes

--- js/login.js
+++ js/login.js
@@ -1,41 +1,22 @@
 function login(){
 
-let role=document.getElementById("role").value;
+let role =
+document.getElementById("role").value;
 
-let email=document.getElementById("email").value;
-
-let password=document.getElementById("password").value;
-
-if(role=="" || email=="" || password==""){
-
-alert("Please fill all fields.");
-
-return;
-
-}
-
-if(role=="admin"){
-
+if(role==="admin"){
 window.location.href="admin/dashboard.html";
-
 }
 
-else if(role=="student"){
-
+else if(role==="student"){
 window.location.href="student/dashboard.html";
-
 }
 
-else if(role=="teacher"){
-
-window.location.href="teacher/dashboard.html";
-
-}
-
-else if(role=="parent"){
-
+else if(role==="parent"){
 window.location.href="parent/dashboard.html";
+}
 
+else{
+window.location.href="teacher/dashboard.html";
 }
 
 }
